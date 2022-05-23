4e4bf1d0f4ca5b6f2808fe325671effef1fe85be
Long Dao <long.dao@zodinet.com>
2022-05-23 20:39:11 +0700

    remove banner

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,17 @@ import React from "react";
 import { Banner, FooterBannber, Product } from "../components";
 import { client } from "../lib/client";
 
-const Home = ({ products, banners }) => {
+const Home = ({ products }) => {
   return (
     <div>
-      <Banner banner={banners && banners[0]} />
+      {/* <Banner banner={banners && banners[0]} /> */}
       <div className="products-heading"></div>
       <div className="products-container">
         {products?.map((product) => (
           <Product key={product._id} data={product} />
         ))}
       </div>
-      <FooterBannber data={banners && banners[0]} />
+      {/* <FooterBannber data={banners && banners[0]} /> */}
     </div>
   );
 };
@@ -21,11 +21,11 @@ const Home = ({ products, banners }) => {
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
   const products = await client.fetch(query);
-  const bannerQuery = '*[_type == "banner"]';
-  const banners = await client.fetch(bannerQuery);
-  console.log(banners);
+  // const bannerQuery = '*[_type == "banner"]';
+  // const banners = await client.fetch(bannerQuery);
+  // console.log(banners);
   return {
-    props: { products, banners },
+    props: { products },
   };
 };
 
